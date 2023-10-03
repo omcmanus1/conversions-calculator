281f02ab0ffbbbd767773b23935bbd0e2979b871
omcmanus1 <omcmanus1@gmail.com>
2023-10-03 22:05:01 +0100

    navigation refactor

---
+++ client/src/components/navigation/index.tsx
@@ -0,0 +1,54 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { cn } from "@/utils/shadutils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+type NavigationTab = {
+  path: string;
+  title: string;
+};
+
+export default function Navigation({
+  navigationTabs,
+}: {
+  navigationTabs: Array<NavigationTab>;
+}) {
+  const pathname = usePathname() || "";
+
+  return (
+    <NavigationMenu className="mb-5">
+      <NavigationMenuList>
+        {navigationTabs.map((tab) => {
+          const isSelected = pathname.includes(tab.path);
+          return (
+            <NavigationMenuItem key={`${tab.path}_key`}>
+              <Button variant="outline" className="mr-2" asChild>
+                <Link href={tab.path} legacyBehavior passHref>
+                  <NavigationMenuLink
+                    className={cn(
+                      navigationMenuTriggerStyle(),
+                      isSelected && "underline"
+                    )}
+                  >
+                    {tab.title}
+                  </NavigationMenuLink>
+                </Link>
+              </Button>
+            </NavigationMenuItem>
+          );
+        })}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
