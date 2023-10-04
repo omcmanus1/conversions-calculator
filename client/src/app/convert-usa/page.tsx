3cd0d5d72f29b7947f912e61f8f0349a0b1eb8b7
omcmanus1 <omcmanus1@gmail.com>
2023-10-04 21:50:07 +0100

    basic input logic

--- client/src/app/convert-usa/page.tsx
+++ client/src/app/convert-usa/page.tsx
@@ -1,7 +1,81 @@
 "use client";
 
+import SelectSh from "@/components/select";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { singleInput } from "@/types/inputTypes";
+import { useState } from "react";
 
 export default function Page() {
-  return <Button variant="outline">Convert From Freedom Units</Button>;
+  const [input, setInput] = useState<singleInput>({
+    ingredient: "",
+    inputSystem: "usa",
+    inputUnit: "",
+    outputSystem: "metric",
+    outputUnit: "",
+    type: "",
+    amount: 0,
+  });
+  console.log("🚀 ~ file: page.tsx:19 ~ Page ~ input:", input);
+  const volumeInputs = ["cups", "gallons", "quarts", "pints", "fluid oz"];
+  const weightInputs = ["cups", "lbs", "oz"];
+  const volumeOutputs = ["millilitres", "litres"];
+  const weightOutputs = ["grams", "kg"];
+
+  const handleInput = <K extends keyof singleInput>(
+    property: K,
+    value: singleInput[K]
+  ) => {
+    setInput({
+      ...input,
+      [property]: value === "string" ? value.toLowerCase() : value,
+    });
+  };
+
+  return (
+    <>
+      <Input
+        className="mb-1"
+        type="text"
+        placeholder="Ingredient"
+        onChange={(e) => handleInput("ingredient", e.target.value)}
+      />
+      {input.ingredient && (
+        <SelectSh
+          handleChange={(e) => handleInput("type", e)}
+          placeholder="Type"
+          selectContent={["weight", "volume"]}
+        />
+      )}
+      {input.type && (
+        <SelectSh
+          handleChange={(e) => handleInput("inputUnit", e)}
+          placeholder="Input Unit"
+          selectContent={
+            input.type === "weight"
+              ? weightInputs
+              : input.type === "volume"
+              ? volumeInputs
+              : ["Please specify a unit"]
+          }
+        />
+      )}
+      {input.inputUnit && (
+        <SelectSh
+          handleChange={(e) => handleInput("outputUnit", e)}
+          placeholder="Output Unit"
+          selectContent={
+            input.type === "weight"
+              ? weightOutputs
+              : input.type === "volume"
+              ? volumeOutputs
+              : ["Please specify a unit"]
+          }
+        />
+      )}
+      <Button className="mt-3" disabled variant="outline">
+        Convert From Freedom Units
+      </Button>
+    </>
+  );
 }
---
+++ client/src/types/inputTypes.ts
@@ -0,0 +1,9 @@
+export type singleInput = {
+  ingredient: string;
+  inputSystem: string;
+  inputUnit: string;
+  outputSystem: string;
+  outputUnit: string;
+  type: string;
+  amount: number;
+};
