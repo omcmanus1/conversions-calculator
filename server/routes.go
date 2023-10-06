9c07461bd20e2030ce01d01020064cfe8531a389
omcmanus1 <omcmanus1@gmail.com>
2023-10-06 22:54:02 +0100

    add cors middleware

--- server/routes.go
+++ server/routes.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/omcmanus1/converter/data"
 )
 
@@ -13,6 +14,14 @@ func SetupRoutes() *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 	r.Get("/api", Home)
 	r.Get("/api/convert/get-encode", GetHandlerEncode)
 	r.Get("/api/convert/get-marshal", GetHandlerMarshal)
