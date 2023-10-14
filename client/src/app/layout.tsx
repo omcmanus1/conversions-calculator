d52fcfa26a754db852b36b1d7f153210f4433eda
omcmanus1 <omcmanus1@gmail.com>
2023-10-14 12:26:32 +0100

    favicon attribution

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
     // { title: "Home", path: "/home" },
     { title: "Convert US To Metric", path: "/recipes/convert-usa" },
     { title: "Convert Metric To US", path: "/recipes/convert-metric" },
+    { title: "Convert List", path: "/recipes/convert-list" },
   ];
   return (
     <html lang="en">
@@ -22,13 +23,22 @@ export default function RootLayout({
         <title>Recipe Unit Converter</title>
       </head>
       <body
-        className={`flex flex-col items-center bg-slate-200 ${inter.className}`}
+        className={`flex flex-col h-full items-center bg-slate-200 ${inter.className}`}
       >
         <header className="m-5 flex flex-col items-center">
           <h1 className="m-5 text-6xl uppercase">Recipe Unit Converter</h1>
           <Navigation navigationTabs={navigationTabs}></Navigation>
         </header>
         <div>{children}</div>
+        <footer className="w-full text-center p-1 absolute bottom-0 bg-slate-200">
+          <a
+            href="https://www.flaticon.com/free-icons/change"
+            title="change icons"
+            className="text-slate-400 text-xs"
+          >
+            Change icons created by Smashicons - Flaticon
+          </a>
+        </footer>
       </body>
     </html>
   );
