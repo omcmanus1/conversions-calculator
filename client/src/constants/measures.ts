9c2e04b999c8cdcfa74f41c8b6b53af3d46b06bd
omcmanus1 <omcmanus1@gmail.com>
2023-10-29 18:56:07 +0000

    add types

--- client/src/constants/measures.ts
+++ client/src/constants/measures.ts
@@ -1,4 +1,17 @@
-export const US_VOLUME = ["cups", "gallons", "quarts", "pints", "fluid oz"];
-export const US_WEIGHT = ["cups", "lbs", "oz"];
-export const METRIC_VOLUME = ["millilitres", "litres"];
-export const METRIC_WEIGHT = ["grams", "kg"];
+import {
+  metricVolume,
+  metricWeight,
+  usVolume,
+  usWeight,
+} from "./../types/conversionTypes";
+
+export const US_VOLUME: usVolume = [
+  "cups",
+  "gallons",
+  "quarts",
+  "pints",
+  "fluid oz",
+];
+export const US_WEIGHT: usWeight = ["cups", "lbs", "oz"];
+export const METRIC_VOLUME: metricVolume = ["millilitres", "litres"];
+export const METRIC_WEIGHT: metricWeight = ["grams", "kg"];
