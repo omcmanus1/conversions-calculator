714734ad6df80f009ab35a43c3d2fd8b158dd7ba
omcmanus1 <omcmanus1@gmail.com>
2023-11-19 13:26:42 +0000

    metric > feet conversion with interface

--- server/heightConversions.go
+++ server/heightConversions.go
@@ -6,13 +6,20 @@ import (
 	"github.com/omcmanus1/converter/types"
 )
 
-func FromFeet(inp types.HeightFeet) (types.HeightMetric, error) {
-	var output types.HeightMetric
+func FromFeet(inp types.HeightFeet) (types.HeightCentimetres, error) {
+	var output types.HeightCentimetres
+
 	if inp.Feet < 0 || inp.Inches < 0 || inp.Inches >= 12 {
 		return output, errors.New("invalid input")
 	}
 	feet := 30.48 * inp.Feet
 	inches := 2.54 * inp.Inches
+
 	output.Centimetres = feet + inches
 	return output, nil
+}
+
+func FromMetric(inp types.HeightFromMetric) (types.HeightFeet, error) {
+	output, err := inp.GetHeightInFeet()
+	return output, err
 }
--- server/types/types.go
+++ server/types/types.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"errors"
+	"math"
+)
+
 type RecipeInput struct {
 	Ingredient   string
 	InputSystem  string
@@ -16,11 +21,49 @@ type RecipeOutput struct {
 	Amount     float64 `json:"amount"`
 }
 
+type HeightFromMetric interface {
+	GetHeightInFeet() (HeightFeet, error)
+}
+
 type HeightFeet struct {
 	Feet   float32 `json:"feet"`
 	Inches float32 `json:"inches"`
 }
 
-type HeightMetric struct {
+type HeightCentimetres struct {
 	Centimetres float32 `json:"centimetres"`
+}
+
+type HeightMetres struct {
+	Metres float32 `json:"metres"`
+}
+
+func (h HeightCentimetres) GetHeightInFeet() (HeightFeet, error) {
+	var output HeightFeet
+
+	if h.Centimetres < 0 {
+		return output, errors.New("invalid input")
+	}
+	totalInches := float64(h.Centimetres) / 2.54
+	feet := math.Floor(totalInches / 12)
+	remainingInches := math.Mod(totalInches, 12)
+	output.Feet = float32(feet)
+	output.Inches = float32(remainingInches)
+
+	return output, nil
+}
+
+func (h HeightMetres) GetHeightInFeet() (HeightFeet, error) {
+	var output HeightFeet
+
+	if h.Metres < 0 {
+		return output, errors.New("invalid input")
+	}
+	totalInches := float64(h.Metres / 0.0254)
+	feet := math.Floor(totalInches / 12)
+	remainingInches := math.Mod(totalInches, 12)
+	output.Feet = float32(feet)
+	output.Inches = float32(remainingInches)
+
+	return output, nil
 }
