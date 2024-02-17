20134323b491d3b93faa606e264351aed3cc6cda
omcmanus1 <omcmanus1@gmail.com>
2024-02-17 13:25:30 +0000

    fix setup errors

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
 	"github.com/omcmanus1/converter"
@@ -12,14 +11,11 @@ import (
 
 func main() {
 	router := converter.SetupRoutes()
-	port := "8090"
-	if envPort := os.Getenv("PORT"); envPort != "" {
-		port = envPort
-		fmt.Printf("Listening on port https://localhost%v...\n", port)
-		err := http.ListenAndServe(port, router)
-		if err != nil {
-			log.Fatal("failed to start server: %w", err)
-		}
+	port := ":8080"
+	fmt.Printf("Listening on port https://localhost%v...\n", port)
+	err := http.ListenAndServe(port, router)
+	if err != nil {
+		log.Fatal("failed to start server: %w", err)
 	}
 	if err := funcframework.Start(port); err != nil {
 		log.Fatalf("funcframework.Start: %v\n", err)
