ce9931d950f98dd5215efda81875a98c929a4bfa
omcmanus1 <omcmanus1@gmail.com>
2024-02-25 17:13:29 +0000

    CORS fuction

--- server/functions.go
+++ server/functions.go
@@ -2,6 +2,7 @@ package converter
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 )
@@ -20,16 +21,7 @@ func GetRequest(w http.ResponseWriter, r *http.Request) {
 }
 
 func PostListHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodOptions {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Max-Age", "3600")
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
+	setHeaders(w, r)
 	PostConversions(w, r)
 }
 
@@ -87,4 +79,16 @@ func PostVolumeMetricHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	PostVolumeMetric(w, r)
+}
+
+func setHeaders(w http.ResponseWriter, r *http.Request) http.ResponseWriter {
+	corsOrigin := os.Getenv("CORS_ORIGIN")
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", corsOrigin)
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+	}
+	return w
 }
