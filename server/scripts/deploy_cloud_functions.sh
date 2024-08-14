fabfb871d02e0efd5f9b48e3ffe510333a41f930
omcmanus1 <omcmanus1@gmail.com>
2024-08-14 23:02:06 +0100

    refactor

--- server/scripts/deploy_cloud_functions.sh
+++ server/scripts/deploy_cloud_functions.sh
@@ -9,26 +9,25 @@ RED="\033[0;31m"
 NO_COLOUR="\033[0m"
 
 # Source environment variables
-if [ ! -f ../.env.local ]; then
+if [[ ! -f ../.env.local ]]; then
   echo "Error: env.local file not found!"
   exit 1
 fi
-# Execute file to set env variable
 . ../.env.local
+
 # Check CORS_ORIGIN is set
-if [ -z "$CORS_ORIGIN" ]; then
+if [[ -z "$CORS_ORIGIN" ]]; then
   echo "Error: CORS_ORIGIN is not set!"
   exit 1
 fi
+
 # Ensure target files exist
-if [ ! -f ../routes.go ]; then
-  echo "Error: routes.go file not found!"
-  exit 1
-fi
-if [ ! -f ../functions.go ]; then
-  echo "Error: functions.go file not found!"
-  exit 1
-fi
+for file in ../routes.go ../functions.go; do
+  if [[ ! -f $file ]]; then
+    echo "Error: $file file not found!"
+    exit 1
+  fi
+done
 
 # Read all existing functions
 existing_funcs=()
@@ -38,15 +37,15 @@ done < <(grep 'functions\.HTTP' ../functions.go)
 
 add_func_if_missing() {
   # Search existing functions for match with entry point
-  found=false
-  for i in "${!existing_funcs[@]}"; do
-    if [[ "${existing_funcs[$i]}" == *"$entry_point"* ]]; then
+  local found=false
+  for func in "${existing_funcs[@]}"; do
+    if [[ "$func" == *"$entry_point"* ]]; then
       found=true
       break
     fi
   done
   # Add function to 'functions.go' file and proceed with deployment
-  if [ "$found" = false ]; then
+  if [[ "$found" == false ]]; then
     echo -e "${RED}!!!!!!!!!!!!!!!!!!${NO_COLOUR}"
     echo "Entry point $entry_point missing in functions list"
     echo -e "${RED}!!!!!!!!!!!!!!!!!!${NO_COLOUR}"
@@ -64,32 +63,34 @@ echo -e "${LIGHT_CYAN}>---------------------------------------------------------
 pids=()
 names=()
 
+# Ensure log directory exists
+log_dir="../scripts/logs/deploy_cloud_functions"
+mkdir -p "$log_dir"
+
 # Read all `r.Post()` routes
 while IFS= read -r line; do
-  # Parse name from first argument
+  # Parse name and entry-point
   name=$(echo "$line" | awk -F'api/ *' '{print $2}' | awk -F'["]' '{print $1}')
-  # Parse entry-point from second argument
   entry_point=$(echo "$line" | awk -F', *' '{print $2}' | awk -F'[)]' '{print $1}')
 
-  if [ -z "$name" ] || [ -z "$entry_point" ]; then
+  if [[ -z "$name" || -z "$entry_point" ]]; then
     echo -e "${RED}Warning: Skipping malformed line: $line ${NO_COLOUR}"
     continue
   fi
 
   add_func_if_missing
 
-  echo "deploying '$name' with entrypoint '$entry_point'..."
+  echo "Deploying '$name' with entrypoint '$entry_point'..."
 
   command="gcloud functions deploy $name --region=europe-west2 --trigger-http --runtime=go121 --gen2 --source=../. --entry-point=$entry_point --allow-unauthenticated --set-env-vars CORS_ORIGIN=$CORS_ORIGIN"
 
   # Execute the command in the background and capture the Process ID
-  eval "$command" >"../scripts/logs/deploy_cloud_functions/${name}.log" 2>&1 &
+  eval "$command" >"$log_dir/${name}.log" 2>&1 &
   pid=$!
-  echo "Output will be logged in 'deploy_cloud_functions/${name}.log'"
+  echo "Output will be logged in '$log_dir/${name}.log'"
   echo -e "${LIGHT_CYAN}>---------------------------------------------------------------------< ${NO_COLOUR}"
 
   # Track background jobs
-  # Store the PID and function name in arrays
   pids+=("$pid")
   names+=("$name")
 done < <(grep 'r\.Post' ../routes.go)
@@ -98,12 +99,12 @@ done < <(grep 'r\.Post' ../routes.go)
 for i in "${!pids[@]}"; do
   pid="${pids[$i]}"
   name="${names[$i]}"
-  log_file="../scripts/logs/deploy_cloud_functions/${name}.log"
+  log_file="$log_dir/${name}.log"
 
   wait "$pid"
   status=$?
 
-  if [ $status -eq 0 ]; then
+  if [[ $status -eq 0 ]]; then
     echo "Deployment for '$name' was successful."
   else
     echo -e "${RED}!!!!!!!!!!!!!!!!!!"
