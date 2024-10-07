1dcc0ccd28d00b94bfc44d0ead8ea17c7a3afbf7
omcmanus1 <omcmanus1@gmail.com>
2024-10-07 23:31:03 +0100

    Remove cups from weight conversions
    - inaccurate due to ingredient variation

--- client/src/constants/measures.ts
+++ client/src/constants/measures.ts
@@ -1,4 +1,4 @@
 export const US_VOLUME = ["cups", "gallons", "quarts", "pints", "fluid oz"];
-export const US_WEIGHT = ["cups", "lbs", "oz"];
+export const US_WEIGHT = ["lbs", "oz"];
 export const METRIC_VOLUME = ["millilitres", "litres"];
 export const METRIC_WEIGHT = ["grams", "kg"];
